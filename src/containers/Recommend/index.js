01bf821344ceb28f9805ede4972b0cd33b8f886a
jizhi <theniceangel@163.com>
2018-03-14 21:52:12 +0800

    完成recommend组件

--- src/containers/Recommend/index.js
+++ src/containers/Recommend/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.stylus'
 import Slider from 'src/base/Slider'
+import Scroll from 'src/base/Scroll'
 import { getRecommend, getDiscList } from 'src/api/recommend'
 import { ERR_OK } from 'src/api/config'
 
@@ -8,13 +9,14 @@ export default class Recommend extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      sliderData: [],
+      sliderList: [],
       recommendList: []
     }
   }
 
   componentWillMount () {
     this._getRecommend()
+    this._getDiscList()
   }
 
   _getRecommend () {
@@ -22,37 +24,50 @@ export default class Recommend extends React.Component {
       console.log(res)
       if (res.code === ERR_OK) {
         this.setState({
-          sliderData: res.data.slider
+          sliderList: res.data.slider
         })
       }
     })
   }
+
   _getDiscList () {
     getDiscList().then((res) => {
       console.log(res)
       if (res.code === ERR_OK) {
         this.setState({
-          recommendList: res.data.recommendList
+          recommendList: res.data.list
         })
       }
     })
   }
+
   render () {
-    let {sliderData} = this.state
+    let {sliderList, recommendList} = this.state
     return (
-      <div>
-        <div className="slider-wrapper">
-          <div className="slider-content">
-            <Slider>
-              {sliderData.map((data) => <div className="slider-img-box" key={data.id}><img src={data.picUrl} alt=""/>
-              </div>)}
-            </Slider>
+      <Scroll>
+        <div className="scroll-wrapper">
+          <div className="slider-wrapper">
+            <div className="slider-content">
+              <Slider>
+                {sliderList.map((data) => <div className="slider-img-box" key={data.id}><img src={data.picUrl} alt=""/>
+                </div>)}
+              </Slider>
+            </div>
+          </div>
+          <div className="recommend-container">
+            <h1 className='recommend-title'>热门歌单</h1>
+            <ul>
+              {recommendList.map((data) => <li className="item" key={data.dissid}>
+                <div className="icon"><img src={data.imgurl} alt="" width='60px' height='60px'/></div>
+                <div className="text">
+                  <h2 className="name">{data.creator.name}</h2>
+                  <p className="desc">{data.dissname}</p>
+                </div>
+              </li>)}
+            </ul>
           </div>
         </div>
-        <div className="recommend-container">
-          <h1 className='recommend-title'>热门歌单</h1>
-        </div>
-      </div>
+      </Scroll>
     )
   }
 }
